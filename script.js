969452e10e16d5d8c93c254ebe8f81120af1011c
shoknt <153351426+shoknt@users.noreply.github.com>
2023-12-13 21:29:39 +0700

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["SHOK", "Programmer", "Web Developer", "modern warsip Player" ],
+        strings: ["SHOK", "Programmer", "Web Developer", "co-founder" ],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["SHOK", "Programmer", "Web Developer", "modern warsip Player",],
+        strings: ["SHOK", "Programmer", "Web Developer", "co-founder",],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
